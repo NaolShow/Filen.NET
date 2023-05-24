d468114878b7b3171ca6f34dbc803cf9efdc6ccb
Loan Jouhannet <loan.jouhannet@protonmail.com>
2023-05-24 20:54:12 +0200

    feat(api): add empty trash route

---
+++ Filen.API/Routes/Trash/TrashEmptyRoute.cs
@@ -0,0 +1,18 @@
+﻿namespace Filen.API {
+
+    public partial class FilenAPI {
+
+        /// <summary>
+        /// Points towards the route <see href="/v3/trash/empty"/> with a random <see cref="FilenDomains.Gateway"/>
+        /// </summary>
+        public static Uri TrashEmptyRoute => new Uri(FilenDomains.Gateway, "/v3/trash/empty");
+
+        /// <summary>
+        /// Empties the trash from any files and folders (cannot be restored!)
+        /// </summary>
+        public async Task TrashEmpty()
+            => await HttpClient.PostAsync(TrashEmptyRoute, null);
+
+    }
+
+}
